846a18d7066250a31f3bade0b4b3f3464b73dbe1
Antônio <a.sousa.jnri@gmail.com>
2023-01-17 08:12:46 -0300

    Add html to app and js to consumer stream from server

---
+++ app/index.js
@@ -0,0 +1,37 @@
+const API_URL = 'http://localhost:3000';
+
+async function consumeAPI(signal) {
+  const response = await fetch(API_URL, {
+    signal
+  });
+  let counter = 0;
+  const reader = response.body
+    .pipeThrough(new TextDecoderStream())
+    .pipeThrough(parseNDJSON())
+    .pipeTo(new WritableStream({
+      write(chunk) {
+        console.log(++counter, 'chunk', chunk);
+      }
+    }));
+  return reader;
+};
+
+function parseNDJSON() {
+  let ndjsonBuffer = '';
+  return new TransformStream({
+    transform(chunk, controller) {
+      ndjsonBuffer += chunk;
+      const items = ndjsonBuffer.split('\n');
+      items.slice(0, -1)
+        .forEach(item => controller.enqueue(JSON.parse(item)));
+      ndjsonBuffer = items[items.length -1];
+    },
+    flush(controller) {
+      if(!ndjsonBuffer) return;
+      controller.enqueue(JSON.parse(ndjsonBuffer));
+    }
+  })
+};
+
+const abortController = new AbortController();
+await consumeAPI(abortController.signal);
